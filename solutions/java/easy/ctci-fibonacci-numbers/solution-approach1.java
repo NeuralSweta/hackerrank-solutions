1ca4f987bf4f0252939ddc0477ad0c60676d96eb
Sweta Jha <swetajhaofficial11@gmail.com>
2026-07-17 10:40:25 +0530

    ✅ Recursion: Fibonacci Numbers [java] — approach1

---
+++ solutions/java/easy/ctci-fibonacci-numbers/solution-approach1.java
@@ -0,0 +1,37 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem?isFullScreen=true
+// Problem     Recursion: Fibonacci Numbers
+// Difficulty  Easy
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-07-17, 10:40 a.m.
+// ──────────────────────────────────────────────────
+
+import java.util.*;
+
+public class Solution {
+
+    public static int fibonacci(int n) {
+        // Complete the function.
+       
+        int[] dp= new int[n+1];
+      return fibo(n,dp);
+    }
+
+    public static int fibo(int n, int[] dp){
+        if(n==0 || n==1)return n;
+        if(dp[n]!=0)return dp[n];
+        dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+        
+    }
+}
